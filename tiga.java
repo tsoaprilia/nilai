dc46d472ca2f04837912d267dc9134f5b50d1286
tsoaprilia <tsoaprilia@gmail.com>
2020-03-05 04:17:12 +0700

    ya commit

--- tiga.java
+++ tiga.java
@@ -21,35 +21,53 @@ public class tiga  extends lima{
          String kelas []=new String[org];
          String mapel[]=new String[org];
          String po[]=new String[org];
+         int nilai1[]=new int [org];
+         int nilai2[]=new int[org];
          int nilai[]=new int[org];
+         int nilaiul2[]=new int[org];
+         int pts[]=new int[org];
+         int rata[]=new int[org];
          
          System.out.println(" ");
          for(int b=0;b<org;b++){
              System.out.print("Nama anak ke "+(b+1)+"    = ");
-             nama[b] =baca.next();
-             nama[b] =baca.nextLine();
+             mapel[b]=baca.nextLine();
+             nama[b]=baca.next();
+          
          }
          System.out.println(" ");
          for(int b=0;b<org;b++){
              System.out.print("kelas anak ke "+(b+1)+"    = ");
+             mapel[b]=baca.nextLine();
              kelas[b] =baca.next();
-             kelas[b] =baca.nextLine();
+           
          }
          System.out.println(" ");
         for(int b=0;b<org;b++){
             System.out.print("Mapel anak ke "+(b+1)+"    = ");
-            mapel[b]=baca.next();
+            
             mapel[b]=baca.nextLine();
+            mapel[b]=baca.next();
         }
          System.out.println(" ");
         for(int b=0;b<org;b++){
-            System.out.print("Nilai anak ke "+(b+1)+"    = ");
+            System.out.println("===============================================");
+            System.out.print("Nilai ulangan-1 anak ke "+(b+1)+"    = ");
             nilai[b]=baca.nextInt();
+            System.out.print("Nilai ulangan-2 anak ke "+(b+1)+"    = ");
+            nilaiul2[b]=baca.nextInt();
+            System.out.print("Nilai harian-1 anak ke "+(b+1)+"     = ");
+            nilai1[b]=baca.nextInt();
+            System.out.print("Nilai harian-2 anak ke "+(b+1)+"     = ");
+            nilai2[b]=baca.nextInt();
+            System.out.print("Nilai pts anak ke "+(b+1)+"          = ");
+            pts[b]=baca.nextInt();
+            rata[b]=(nilai[b]+nilaiul2[b]+nilai1[b]+nilai2[b]+pts[b])/5;
         }
          System.out.println(" ");
         for(int b=0;b<org;b++){
             
-            if(nilai[b]>=70){
+            if(rata[b]>=70){
                 po[b]="LULUS";
             }else if(nilai[b]<70){
                 po [b]="TIDAK LULUS";
@@ -60,18 +78,18 @@ public class tiga  extends lima{
         
         System.out.println(" ");
         System.out.println(" ");
-        System.out.println("===================================================================================");
+        System.out.println("============================================================================================================================================================================================================================");
         System.out.println(" ");
-        System.out.println("         PROGRAM PENENTUAN KELULUSAN");
-        System.out.println(" semester ganjil tahun ajaran 2019/2020");
-        System.out.println("              SMK TELKOM MALANG");
+        System.out.println("           PROGRAM PENENTUAN KELULUSAN");
+        System.out.println("   semester ganjil tahun ajaran 2019/2020");
+        System.out.println("                SMK TELKOM MALANG");
         System.out.println(" ");
-        System.out.println("no   |  nama        | kelas | mapel |  nilai | predikat  |  ");    
+        System.out.println("no         |  nama           | kelas       | mapel      |  nilai       | predikat      |  ");    
         for(int d=0;d<org;d++){
-        System.out.println(((d+1)+"    |"+nama[d]    +"         |" +kelas[d]+"    | "+mapel[d]+"     | " +nilai[d]+"   |"+po[d] ) );
+        System.out.println(((d+1)+"            "+nama[d]    +"              " +kelas[d]+"          "+mapel[d]+"            " +rata[d]+"            "+po[d] ) );
         }
         System.out.println(" ");
-        System.out.println("====================================================================================");
+        System.out.println("================================================================================================================================================================================================================================");
         System.out.println(" ");
         System.out.println(" ");
         }
